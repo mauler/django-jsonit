442e75a114e6477d1fc08fb3d84746d5a0694f14
Paulo R. Macedo Chaves <proberto.macedo@gmail.com>
2012-09-21 02:11:35 -0300

    Add support to return traceback.
    There is a settings values JSONIT_TRACEBACK that holds
    the option to return the traceback (more information) than
    the exception (no useful information) on decorator
    catch_ajax_exceptions

--- jsonit/decorators.py
+++ jsonit/decorators.py
@@ -1,23 +1,34 @@
 from functools import wraps
+import traceback
+
+from django.conf import settings
 
 from jsonit.http import JSONResponse
 
 
+JSONIT_TRACEBACK = getattr(settings, "JSONIT_TRACEBACK", False)
+
+
 def catch_ajax_exceptions(func):
     """
-    Catches exceptions which occur when using an AJAX request. 
-    
+    Catches exceptions which occur when using an AJAX request.
+
     These exceptions will be returned using a :class:`JSONResponse` rather than
     letting the exception propogate.
     """
-    
+
     @wraps(func)
     def dec(request, *args, **kwargs):
         try:
             return func(request, *args, **kwargs)
         except Exception, e:
             if request.is_ajax():
-                return JSONResponse(request, exception=e)
+                if JSONIT_TRACEBACK:
+                    return JSONResponse(
+                        request, exception=traceback.format_exc())
+                else:
+                    return JSONResponse(request, exception=e)
             raise
 
     return dec
+
